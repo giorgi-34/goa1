06638fc19aa7c81ed0ae3c5ec2c665aa6afedbc1
giorgi-34 <giorgigachechiladze50@gmail.com>
2024-11-09 12:02:36 -0800

    commit

---
+++ Level 10/classwork/task03.py
@@ -0,0 +1,42 @@
+
+# მეტი (>) ოპერატორი
+print(5 > 3)     # True
+print(10 > 15)   # False
+print(7 > 7)     # False
+print(20 > 10)   # True
+print(0.5 > 0.1) # True
+
+# ნაკლები (<) ოპერატორი
+print(3 < 5)     # True
+print(15 < 10)   # False
+print(7 < 7)     # False
+print(10 < 20)   # True
+print(0.1 < 0.5) # True
+
+# მეტი ან ტოლი (>=) ოპერატორი
+print(5 >= 3)    # True
+print(10 >= 15)  # False
+print(7 >= 7)    # True
+print(20 >= 10)  # True
+print(0.5 >= 0.5)# True
+
+# ნაკლები ან ტოლი (<=) ოპერატორი
+print(3 <= 5)    # True
+print(15 <= 10)  # False
+print(7 <= 7)    # True
+print(10 <= 20)  # True
+print(0.1 <= 0.5)# True
+
+# ტოლი (==) ოპერატორი
+print(5 == 5)    # True
+print(10 == 15)  # False
+print(7 == 7)    # True
+print("a" == "a")# True
+print(3.5 == 3.5)# True
+
+# არ არის ტოლი (!=) ოპერატორი
+print(5 != 3)    # True
+print(10 != 10)  # False
+print("a" != "b")# True
+print(3.5 != 4.0)# True
+print(4 != 3)    # True
