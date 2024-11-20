de1d88355803d3ae6f3380b17215715e86056928
giorgi-34 <giorgigachechiladze50@gmail.com>
2024-11-20 01:47:29 -0800

    commit

---
+++ Level 14/classwork/task03.py
@@ -0,0 +1,6 @@
+# Ask the user to enter a number
+n = int(input("1 (n): "))
+
+# Use a for loop to print the squares of numbers from 1 to n
+for i in range(1, n + 1):
+    print(f"The square of {i} is {i ** 2}")
