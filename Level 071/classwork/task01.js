c52d0779e1fe578a68a32e08179d12ccf14e7911
giorgi-34 <giorgigachechiladze50@gmail.com>
2025-06-10 10:13:07 -0700

    commit

---
+++ Level 071/classwork/task01.js
@@ -0,0 +1,3 @@
+let fruits = ['apple', 'banana', 'orange'];
+console.log(fruits.length);         // შედეგი: 3
+console.log(fruits.concat(['kiwi', 'melon'])); // ['apple', 'banana', 'orange', 'kiwi', 'melon']
